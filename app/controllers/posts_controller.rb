78370a5a8ba0a669e904ee4ceae20b75270e31ee
vincent <vincentuong@yahoo.com>
2021-11-02 00:16:28 -0500

    commit tree 64bfeb77e10c662a3a43018cda616faa002818fe

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,31 @@
 class PostsController < ApplicationController
-  def index
-  	@posts = Post.all
-  end
+	def index
+		@posts = Post.all
+	end
 
-  def show
-  	@post = Post.find(params[:id])
-  end
+	def show
+		@post = Post.find(params[:id])
+	end
 
-  def new
-  	@post = Post.new
-  end
+	def new
+		@post = Post.new
+	end
 
-  def create
-    @post = Post.new(params)
-    @post.save
-    redirect_to post_path(@post)
-  end
+	def create
+	  @post = Post.new
+	  @post.title = params[:title]
+	  @post.description = params[:description]
+	  @post.save
+	  redirect_to post_path(@post)
+	end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(post_params(:title))
-    redirect_to post_path(@post)
-  end
+	def edit
+		@post = Post.find(params[:id])
+	end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  private
-
-  def post_params(*args)
-    params.require(:post).permit(*args)
-  end
+	def update
+	  @post = Post.find(params[:id])
+	  @post.update(params.require(:post).permit(:title, :description))
+	  redirect_to post_path(@post)
+	end
 end
